79fe95f4d8554dcdcbdcbf6ec944d07d47bbb767
Awerito <diego@openlink.cl>
2024-09-11 20:05:11 -0300

    Add: form example

---
+++ 04-forms/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CustomForm from './components/CustomForm'
+
+function App() {
+  return (
+    <>
+      <h1>React Form</h1>
+      <CustomForm />
+    </>
+  )
+}
+
+export default App
---
+++ 04-forms/src/components/CustomForm.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+const CustomForm = () => {
+  const [formData, setFormData] = useState({ name: '', email: '' });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Name: ${formData.name}, Email: ${formData.email}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default CustomForm;
+
